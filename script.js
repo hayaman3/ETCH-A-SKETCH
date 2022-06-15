6aeb59aa07b9b7e09c4202f12984a078cb3942ab
Kevin <kevin.haya@yahoo.com>
2022-06-15 22:12:37 +0800

    created changeColor

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (i = 0; i < (rows * cols); i++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
+    let cell = document.createElement('div');
+    cell.classList.add('grid-item');
+    cell.addEventListener('mouseover', changeColor)
+    cell.addEventListener('mousedown', changeColor)
+
+    container.appendChild(cell)
   };
 };
 
 makeRows(16, 16);
+
+var mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function changeColor(e){
+  if (e.type === 'mouseover' && !mouseDown) return
+
+  e.target.style.backgroundColor = "black"
+}
