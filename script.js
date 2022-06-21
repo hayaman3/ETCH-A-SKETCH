4f202e8320e783c6ceb5d714b23e88dc55dbac29
Kevin <kevin.haya@yahoo.com>
2022-06-21 23:01:38 +0800

    added buttons for change colorMode

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var columnInput = document.getElementById("column");
 const defaultSize = "16";
 var rows = defaultSize;
 var cols = defaultSize;
+var colorMode = "black";
 
 function setGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
@@ -12,8 +13,8 @@ function setGrid(rows, cols) {
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement('div');
     cell.classList.add('grid-item');
-    cell.addEventListener('mouseover', changeColor)
-    cell.addEventListener('mousedown', changeColor)
+    cell.addEventListener('mouseover', paint)
+    cell.addEventListener('mousedown', paint)
 
     container.appendChild(cell)
   };
@@ -26,11 +27,21 @@ var mouseDown = false
 container.onmousedown = () => (mouseDown = true)
 container.onmouseup = () => (mouseDown = false)
 
-function changeColor(e){
-  if (e.type === 'mouseover' && !mouseDown){
-    return
+function paint(e){
+  if(colorMode==="black"){
+    if (e.type === 'mouseover' && !mouseDown){
+      return
+    }
+    e.target.style.backgroundColor = "black"
   }
-  e.target.style.backgroundColor = "black"
+  if(colorMode=="red"){
+    e.target.style.backgroundColor = "red"
+  }
+}
+
+function changeColor(color){
+  colorMode = color;
+  console.log(color)
 }
 
 rowInput.onchange = () => {
@@ -50,3 +61,4 @@ function resetGrid(){
 function clearGrid(){
   container.innerHTML = '';
 }
+
